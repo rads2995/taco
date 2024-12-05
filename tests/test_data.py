cb3139e8ca96bb6b93173f8e71acac36fb9a1e06
Rodrigo Duran <rads2995@gmail.com>
2024-12-04 23:05:25 -0500

    Adding unit-test to test all data (takes forever).

--- tests/test_data.py
+++ tests/test_data.py
@@ -36,115 +36,26 @@ class TestData(unittest.TestCase):
         taquito.run()
         self.assertAlmostEqual(float(taquito.run()[1]), 7293.0)
 
-    def test_small_cities(self):
+    def test_all_data(self):
 
-        dist_matrix_file = np.loadtxt(self.filepath.joinpath('small_cities.csv'),
-        dtype=str,
-        delimiter=',',
-        encoding='utf-8-sig')
-        dist_matrix = dist_matrix_file[np.any(dist_matrix_file != '', axis=1)].astype(np.float64)
-        taquito: taco = taco(distance_matrix=dist_matrix, num_ants=np.shape(dist_matrix)[0], num_iter=self.num_iter, alpha=self.alpha, beta=self.beta, rho=self.rho, Q=self.Q)
-        taquito.run()
-        self.assertAlmostEqual(float(taquito.run()[1]), 1248.0)
+        for root, dirs, files in self.filepath.walk(on_error=print):
+            for iterator in files:
+                dist_matrix_file = np.loadtxt(self.filepath.joinpath(iterator),
+                dtype=str,
+                delimiter=',',
+                encoding='utf-8-sig')
+                dist_matrix = dist_matrix_file[np.any(dist_matrix_file != '', axis=1)].astype(np.float64)
+                taquito: taco = taco(distance_matrix=dist_matrix, num_ants=np.shape(dist_matrix)[0], num_iter=self.num_iter, alpha=self.alpha, beta=self.beta, rho=self.rho, Q=self.Q)
+                best_path, best_distance = taquito.run()
 
-    def test_medium_cities(self):
-
-        dist_matrix_file = np.loadtxt(self.filepath.joinpath('medium_cities.csv'),
-        dtype=str,
-        delimiter=',',
-        encoding='utf-8-sig')
-        dist_matrix = dist_matrix_file[np.any(dist_matrix_file != '', axis=1)].astype(np.float64)
-        taquito: taco = taco(distance_matrix=dist_matrix, num_ants=np.shape(dist_matrix)[0], num_iter=self.num_iter, alpha=self.alpha, beta=self.beta, rho=self.rho, Q=self.Q)
-        taquito.run()
-        self.assertAlmostEqual(float(taquito.run()[1]), 1194.0)
-
-    def test_big_cities(self):
-
-        dist_matrix_file = np.loadtxt(self.filepath.joinpath('big_cities.csv'),
-        dtype=str,
-        delimiter=',',
-        encoding='utf-8-sig')
-        dist_matrix = dist_matrix_file[np.any(dist_matrix_file != '', axis=1)].astype(np.float64)
-        taquito: taco = taco(distance_matrix=dist_matrix, num_ants=np.shape(dist_matrix)[0], num_iter=self.num_iter, alpha=self.alpha, beta=self.beta, rho=self.rho, Q=self.Q)
-        taquito.run()
-        self.assertAlmostEqual(float(taquito.run()[1]), 27603.0)
-
-    def test_burma14(self):
-
-        dist_matrix_file = np.loadtxt(self.filepath.joinpath('3burma14.csv'),
-        dtype=str,
-        delimiter=',',
-        encoding='utf-8-sig')
-        dist_matrix = dist_matrix_file[np.any(dist_matrix_file != '', axis=1)].astype(np.float64)
-        taquito: taco = taco(distance_matrix=dist_matrix, num_ants=np.shape(dist_matrix)[0], num_iter=self.num_iter, alpha=self.alpha, beta=self.beta, rho=self.rho, Q=self.Q)
-        taquito.run()
-        self.assertAlmostEqual(float(taquito.run()[1]), 3323.0)
-
-    def test_gr17(self):
-
-        dist_matrix_file = np.loadtxt(self.filepath.joinpath('4gr17.csv'),
-        dtype=str,
-        delimiter=',',
-        encoding='utf-8-sig')
-        dist_matrix = dist_matrix_file[np.any(dist_matrix_file != '', axis=1)].astype(np.float64)
-        taquito: taco = taco(distance_matrix=dist_matrix, num_ants=np.shape(dist_matrix)[0], num_iter=self.num_iter, alpha=self.alpha, beta=self.beta, rho=self.rho, Q=self.Q)
-        taquito.run()
-        self.assertAlmostEqual(float(taquito.run()[1]), 2085.0)
-
-    def test_gr21(self):
-
-        dist_matrix_file = np.loadtxt(self.filepath.joinpath('5gr21.csv'),
-        dtype=str,
-        delimiter=',',
-        encoding='utf-8-sig')
-        dist_matrix = dist_matrix_file[np.any(dist_matrix_file != '', axis=1)].astype(np.float64)
-        taquito: taco = taco(distance_matrix=dist_matrix, num_ants=np.shape(dist_matrix)[0], num_iter=self.num_iter, alpha=self.alpha, beta=self.beta, rho=self.rho, Q=self.Q)
-        taquito.run()
-        self.assertAlmostEqual(float(taquito.run()[1]), 2707.0)
-
-    def test_gr24(self):
-
-        dist_matrix_file = np.loadtxt(self.filepath.joinpath('5gr24.csv'),
-        dtype=str,
-        delimiter=',',
-        encoding='utf-8-sig')
-        dist_matrix = dist_matrix_file[np.any(dist_matrix_file != '', axis=1)].astype(np.float64)
-        taquito: taco = taco(distance_matrix=dist_matrix, num_ants=np.shape(dist_matrix)[0], num_iter=self.num_iter, alpha=self.alpha, beta=self.beta, rho=self.rho, Q=self.Q)
-        taquito.run()
-        self.assertAlmostEqual(float(taquito.run()[1]), 1272.0)
-
-    def test_bays29(self):
-
-        dist_matrix_file = np.loadtxt(self.filepath.joinpath('6bays29.csv'),
-        dtype=str,
-        delimiter=',',
-        encoding='utf-8-sig')
-        dist_matrix = dist_matrix_file[np.any(dist_matrix_file != '', axis=1)].astype(np.float64)
-        taquito: taco = taco(distance_matrix=dist_matrix, num_ants=np.shape(dist_matrix)[0], num_iter=self.num_iter, alpha=self.alpha, beta=self.beta, rho=self.rho, Q=self.Q)
-        taquito.run()
-        self.assertAlmostEqual(float(taquito.run()[1]), 2020.0)
-
-    def test_fri26(self):
-
-        dist_matrix_file = np.loadtxt(self.filepath.joinpath('6fri26.csv'),
-        dtype=str,
-        delimiter=',',
-        encoding='utf-8-sig')
-        dist_matrix = dist_matrix_file[np.any(dist_matrix_file != '', axis=1)].astype(np.float64)
-        taquito: taco = taco(distance_matrix=dist_matrix, num_ants=np.shape(dist_matrix)[0], num_iter=self.num_iter, alpha=self.alpha, beta=self.beta, rho=self.rho, Q=self.Q)
-        taquito.run()
-        self.assertAlmostEqual(float(taquito.run()[1]), 937.0)
-
-    def test_dantzig42(self):
-
-        dist_matrix_file = np.loadtxt(self.filepath.joinpath('9dantzig42.csv'),
-        dtype=str,
-        delimiter=',',
-        encoding='utf-8-sig')
-        dist_matrix = dist_matrix_file[np.any(dist_matrix_file != '', axis=1)].astype(np.float64)
-        taquito: taco = taco(distance_matrix=dist_matrix, num_ants=np.shape(dist_matrix)[0], num_iter=self.num_iter, alpha=self.alpha, beta=self.beta, rho=self.rho, Q=self.Q)
-        taquito.run()
-        self.assertAlmostEqual(float(taquito.run()[1]), 699.0)
+                print(' ')
+                print('TSP Instance Name: {:}'.format(iterator))
+                print('Objective: {:.6f}'.format(best_distance))
+                print('Best Route:')
+                print('{:d}'.format(best_path[0]), end=' ')
+                for index in range(1, len(best_path) - 1):
+                    print('-> {:d}'.format(best_path[index]), end=' ')
+                print('-> {:d} -> {:d}'.format(best_path[-1], best_path[0]))
 
 if __name__ == '__main__':
     unittest.main()
